6156905fc3bdf4bbf1f0853b60633f380dd55356
tmsksfh2012 <tmsksfh2012@naver.com>
2022-04-12 13:13:38 +0900

    04.12.2022

---
+++ GongGalBBang/GongGalBBang/GongGalBBang/GongGalBBangApp.swift
@@ -0,0 +1,17 @@
+//
+//  GongGalBBangApp.swift
+//  GongGalBBang
+//
+//  Created by 김택현 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct GongGalBBangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
