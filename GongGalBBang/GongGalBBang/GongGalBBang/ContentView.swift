e3831ec65bfb6b043fb6fef0da428e9a47b3579a
tmsksfh2012 <tmsksfh2012@naver.com>
2022-04-12 14:59:14 +0900

    04.12.2022 (working login || sign up)

--- GongGalBBang/GongGalBBang/GongGalBBang/ContentView.swift
+++ GongGalBBang/GongGalBBang/GongGalBBang/ContentView.swift
@@ -8,10 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var showDetails = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Text("Log In")
+                .font(.title)
+                .padding()
+            
+            VStack {
+                Text("Email address")
+            }
+            
+            Button(action: {
+                print("login tapped!")
+            }) {
+                Text("로그인")
+                    .foregroundColor(.black)
+                    .fontWeight(.semibold)
+                    .font(.title3)
+            }.buttonStyle(LoginStyle())
+        }
     }
+}
+struct LoginStyle: ButtonStyle {
+  func makeBody(configuration: Configuration) -> some View {
+    configuration.label
+      .padding(.vertical, 12)
+      .padding(.horizontal, 45)
+      .foregroundColor(Color.black)
+      .background(Color.white)
+      .cornerRadius(8)
+      .overlay(RoundedRectangle(cornerRadius: 8)
+                .stroke(.black, lineWidth: 2)
+      )
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ GongGalBBang/GongGalBBang/GongGalBBang/splash/Splash.swift
@@ -0,0 +1,8 @@
+//
+//  Splash.swift
+//  GongGalBBang
+//
+//  Created by 김택현 on 2022/04/12.
+//
+
+import Foundation
